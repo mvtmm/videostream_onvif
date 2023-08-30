4dadc673c598cca786ef6310f8b45db6c52693b9
maike <maike.splittgerber@thyssenkrupp.com>
2023-08-30 09:47:02 +0200

    Added support for 2 cameras and improved performance with threading

--- videostream.py
+++ videostream.py
@@ -1,8 +1,15 @@
 from onvif import ONVIFCamera
 import cv2
+import threading    
 
 def zeep_pythonvalue(self, xmlvalue):
     return xmlvalue
+
+def capture_frame(cap, frame_container):
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            frame_container[0] = frame
 
 def connect_to_camera(ip, port, user, password):
     mycam = ONVIFCamera(ip, port, user, password)
@@ -27,31 +34,61 @@ def get_stream_uri(media_service, media_profile):
     return resp.Uri
 
 def main():
-    ip = '192.168.1.67'  # Replace with your camera's IP
+
+    # Data Camera 1 -> K1
+    ip_1 = '192.168.1.135'  
+    port = 80 
+    user = 'admin' 
+    password = '123456'  
+
+    # Data Camera 2 -> K2
+    ip_2 = '192.168.1.67'  # Replace with your camera's IP
     port = 80  # Replace with your camera's port
     user = 'admin'  # Replace with your camera's username
     password = '123456'  # Replace with your camera's password
-    
+
+
+
     # Connect to the camera
-    media_service, media_profile, ptz = connect_to_camera(ip, port, user, password)
+    media_service_c1, media_profile_c1, ptz = connect_to_camera(ip_1, port, user, password)
+    media_service_c2, media_profile_c2, ptz = connect_to_camera(ip_2, port, user, password)
+
+
     
-    # Get Stream URI
-    stream_uri = get_stream_uri(media_service, media_profile)
+# Get Stream URI
+    stream_uri_c1 = get_stream_uri(media_service_c1, media_profile_c1)
+    stream_uri_c2 = get_stream_uri(media_service_c2, media_profile_c2)
     
     # Display video stream
-    cap = cv2.VideoCapture(stream_uri)
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("Failed to get frame")
-            break
+    cap_1 = cv2.VideoCapture(stream_uri_c1)
+    cap_2 = cv2.VideoCapture(stream_uri_c2)
 
-        cv2.imshow('ONVIF Camera Stream', frame)
+    frame_1 = [None]
+    frame_2 = [None]
+
+    # Start threads to capture frames
+    thread_1 = threading.Thread(target=capture_frame, args=(cap_1, frame_1))
+    thread_2 = threading.Thread(target=capture_frame, args=(cap_2, frame_2))
+    thread_1.start()
+    thread_2.start()
+
+    screen_width = 1920
+    screen_height = 1080
+
+    while True:
+        if frame_1[0] is not None and frame_2[0] is not None:
+            # Process and display frames
+                    # Resize frames to fit the window size
+            frame_1_resized = cv2.resize(frame_1[0], (screen_width, screen_height))
+            frame_2_resized = cv2.resize(frame_2[0], (screen_width, screen_height))
+            cv2.imshow('Camera 1', frame_1[0])
+            cv2.imshow('Camera 2', frame_2[0])
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    cap.release()
+    cap_1.release()
+    cap_2.release()
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
